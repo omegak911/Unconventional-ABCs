182f816751bb67a6b03f1d7a0dbe0118a809449d
Kin Chan <xkinchanx@gmail.com>
2018-08-09 15:14:22 -0700

    [feat] some potential functions to play the game

--- client/app.js
+++ client/app.js
@@ -1,6 +1,36 @@
 const myAnimalApp = angular.module('myAnimalApp', []);
 
 myAnimalApp.controller('myAnimalController', ['$scope', ($scope) => {
+  //functions
+  $scope.startGame = (option) => {
+    $scope.gameMode = option;
+    //function to start game
+  }
+
+  
+  $scope.updateIndex = () => {
+    if ($scope.gameMode === 'abc') {
+      $scope.index += 1;
+    } else if  ($scope.gameMode === 'cba') {
+      $scope.index -= 1;
+    } else {
+      $scope.index = Math.floor(Math.random() * $scope.animals.length);
+    }
+  }
+  
+  $scope.checkLetter = (letter) => {
+    //if letter spoken === letter of animal
+  }
+  
+  
+  //data
+  $scope.solvedAnimals = [
+    //upon solving, push solved here to display on the side
+  ]
+
+  $scope.gameMode = 'abc'; //determines how index will be chosen (increment, decrement, random)
+  $scope.index = 0;
+
   $scope.animals = [
     {
       name: 'Aye Aye',
