28cee33816b8f68e5d4941ec9ac82c1a63a58f42
Kin Chan <xkinchanx@gmail.com>
2018-08-09 21:49:23 -0700

    [refactor] moved json data to separate file

---
+++ client/data/data.js
@@ -0,0 +1,34 @@
+[{
+  "name": "Aye Aye",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Wild_aye_aye.jpg/500px-Wild_aye_aye.jpg"
+}, {
+  "name": "Bandicoot",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/8/8b/Perameles_gunni.jpg"
+}, {
+  "name": "Cichlid",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/6/6e/Freshwater_angelfish_biodome.jpg"
+}, {
+  "name": "Dogue De Bordeaux",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/DJT_0086.jpg/800px-DJT_0086.jpg"
+}, {
+  "name": "Elephant Shrew",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Rhynchocyon_petersi_from_side.jpg/800px-Rhynchocyon_petersi_from_side.jpg"
+}, {
+  "name": "Frigate Bird",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/3/34/Male_Frigate_bird.jpg"
+}, {
+  "name": "Green Bee-Eater",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Green_bee-eater_%28Merops_orientalis%29_Photograph_by_Shantanu_Kuveskar.jpg/320px-Green_bee-eater_%28Merops_orientalis%29_Photograph_by_Shantanu_Kuveskar.jpg"
+}, {
+  "name": "Hawaiian Honeycreeper",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/d/d9/Iiwi.jpg"
+}, {
+  "name": "Indri",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Indri_indri_001.jpg/800px-Indri_indri_001.jpg"
+}, {
+  "name": "Jeroba",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/c/c5/Four-toes-jerboa.jpg"
+}, {
+  "name": "Kakapo",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/a/aa/Sirocco_full_length_portrait.jpg"
+}]
