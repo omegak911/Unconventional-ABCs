8af24c4aa7206f83d1d30bfa9b094a09c89f848d
Kin Chan <xkinchanx@gmail.com>
2018-08-11 17:35:32 -0700

    [prog] full list of animals from A-Z

--- client/data/data.js
+++ client/data/data.js
@@ -5,11 +5,11 @@
   "name": "Bandicoot",
   "img": "https://upload.wikimedia.org/wikipedia/commons/8/8b/Perameles_gunni.jpg"
 }, {
-  "name": "Cichlid",
-  "img": "https://upload.wikimedia.org/wikipedia/commons/6/6e/Freshwater_angelfish_biodome.jpg"
+  "name": "Cassowary",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Southern_Cassowary_7071.jpg/220px-Southern_Cassowary_7071.jpg"
 }, {
-  "name": "Dogue De Bordeaux",
-  "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/DJT_0086.jpg/800px-DJT_0086.jpg"
+  "name": "Dugong",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Dugong.JPG/220px-Dugong.JPG"
 }, {
   "name": "Elephant Shrew",
   "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Rhynchocyon_petersi_from_side.jpg/800px-Rhynchocyon_petersi_from_side.jpg"
@@ -20,15 +20,75 @@
   "name": "Green Bee-Eater",
   "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Green_bee-eater_%28Merops_orientalis%29_Photograph_by_Shantanu_Kuveskar.jpg/320px-Green_bee-eater_%28Merops_orientalis%29_Photograph_by_Shantanu_Kuveskar.jpg"
 }, {
-  "name": "Hawaiian Honeycreeper",
-  "img": "https://upload.wikimedia.org/wikipedia/commons/d/d9/Iiwi.jpg"
+  "name": "Horseshoe Crab",
+  "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Limulus_polyphemus.jpg/220px-Limulus_polyphemus.jpg"
 }, {
   "name": "Indri",
   "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Indri_indri_001.jpg/800px-Indri_indri_001.jpg"
 }, {
-  "name": "Jeroba",
+  "name": "Jerboa",
   "img": "https://upload.wikimedia.org/wikipedia/commons/c/c5/Four-toes-jerboa.jpg"
 }, {
-  "name": "Kakapo",
-  "img": "https://upload.wikimedia.org/wikipedia/commons/a/aa/Sirocco_full_length_portrait.jpg"
-}]
+  "name": "Komodo Dragon",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Komodo_dragon_with_tongue.jpg/220px-Komodo_dragon_with_tongue.jpg"
+}, {
+  "name": "Leech",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Sucking_leech.jpg/220px-Sucking_leech.jpg"
+}, {
+  "name": "Manta Ray",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Manta_birostris-Thailand4.jpg/220px-Manta_birostris-Thailand4.jpg"
+}, {
+  "name": "Narwal",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Narwhalsk.jpg/220px-Narwhalsk.jpg"
+}, {
+  "name": "Orca",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Killerwhales_jumping.jpg/220px-Killerwhales_jumping.jpg"
+}, {
+  "name": "Pademelon",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Thylogale_billardierii.jpg/220px-Thylogale_billardierii.jpg"
+}, {
+  "name": "Quoll",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Dasyurus_maculatus.jpg/220px-Dasyurus_maculatus.jpg"
+}, {
+  "name": "Reindeer",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Reinbukken_p%C3%A5_frisk_gr%C3%B8nt_beite._-_panoramio.jpg/220px-Reinbukken_p%C3%A5_frisk_gr%C3%B8nt_beite._-_panoramio.jpg"
+}
+, {
+  "name": "Sungazer",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Cordylus_giganteus.jpg/220px-Cordylus_giganteus.jpg"
+}, {
+  "name": "Tamarin",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Tamarin_portrait_2_edit3.jpg/220px-Tamarin_portrait_2_edit3.jpg"
+}, {
+  "name": "Uncas Skipper",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Hesperia_uncas.jpg/220px-Hesperia_uncas.jpg"
+}
+, {
+  "name": "Vulture",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Vulture_19o05.jpg/220px-Vulture_19o05.jpg"
+}
+, {
+  "name": "Wolverine",
+  "img":  "https://upload.wikimedia.org/wikipedia/en/c/c8/Marvelwolverine.jpg"
+}
+, {
+  "name": "Xenarthra",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Choloepus_hoffmanni_%28Puerto_Viejo%2C_CR%29_crop.jpg/290px-Choloepus_hoffmanni_%28Puerto_Viejo%2C_CR%29_crop.jpg"
+}, {
+  "name": "Yak",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Bos_grunniens_at_Letdar_on_Annapurna_Circuit.jpg/220px-Bos_grunniens_at_Letdar_on_Annapurna_Circuit.jpg"
+}
+, {
+  "name": "Zorse",
+  "img":  "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Zeedonk_800.jpg/220px-Zeedonk_800.jpg"
+}
+
+
+
+
+
+
+
+
+
+]
